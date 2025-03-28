179d5cd8f104129c6c7d6e97b02834dea98f8925
oriana <oriana25salazar@gmail.com>
2025-03-28 18:35:11 -0300

    Aula 05 fundamentos-componentes

---
+++ 05-fundamentos-react-native/componentes/PrimeiroComponente.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default function PrimeiroComponente() {
+    return (
+        <View>
+          <Text style={styles.textoGrande} >Primeiro Componente</Text>
+        </View>
+      )
+    
+    }
+    
+    const styles = StyleSheet.create({
+      textoGrande: {
+        fontSize: 40,
+        fontWeight: 600
+      }
+    })
---
+++ 05-fundamentos-react-native/componentes/SegundoComponente.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text } from "react-native";
+
+
+export default function SegundoComponente() {
+    
+    return <Text>Segundo Componente</Text>
+}
